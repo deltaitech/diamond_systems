d12a3ef07e34a5675ab61d8758421481dd19981a
deltaitech <deltaitech>
2024-01-15 11:20:00 +0200

    Change API url

--- src/utils/API/APi.js
+++ src/utils/API/APi.js
@@ -1,68 +1,71 @@
 import axios from "axios";
 export const baseURL = {
-localhost : "http://127.0.0.1:8000/api/",
-demo : "https://demoyoursite.xyz/mohamed/diamond-systems/public/api/",
-}
-    
+  localhost: "http://127.0.0.1:8000/api/",
+  demo: "https://dse.sa/back/api/",
+};
+
 export const api = axios.create({
-    baseURL: baseURL.demo
-  });
+  baseURL: baseURL.demo,
+});
 
-  export const homeAPI = async (lang, { rejectWithValue }) => {
-    const response = await api
-      .get("home", {
-        headers: {
-          locale: lang,
-        },
-      })
-      .then((res) => {
-        // console.log(res.data);
-        return res.data.data;
-      })
-      .catch((err) => {
-        // console.log(err.response);
-        // return rejectWithValue(err.response.data.data);
-        return rejectWithValue(err.message);
-      });
-    return response;
-  }
+export const homeAPI = async (lang, { rejectWithValue }) => {
+  const response = await api
+    .get("home", {
+      headers: {
+        locale: lang,
+      },
+    })
+    .then((res) => {
+      // console.log(res.data);
+      return res.data.data;
+    })
+    .catch((err) => {
+      // console.log(err.response);
+      // return rejectWithValue(err.response.data.data);
+      return rejectWithValue(err.message);
+    });
+  return response;
+};
 
-  export const serviceAPI = async ({lang, searchParams={}}, { rejectWithValue }) => {
-    const response = await api
-      .get("services", {
-        headers: {
-          locale: lang,
-        },
-        params:{
-          ...searchParams,
-        }
-      })
-      .then((res) => {
-        // console.log(res.data);
-        return res.data.data;
-      })
-      .catch((err) => {
-        // console.log(err.response);
-        // return rejectWithValue(err.response.data.data);
-        return rejectWithValue(err.message);
-      });
-    return response;
-  }
+export const serviceAPI = async (
+  { lang, searchParams = {} },
+  { rejectWithValue }
+) => {
+  const response = await api
+    .get("services", {
+      headers: {
+        locale: lang,
+      },
+      params: {
+        ...searchParams,
+      },
+    })
+    .then((res) => {
+      // console.log(res.data);
+      return res.data.data;
+    })
+    .catch((err) => {
+      // console.log(err.response);
+      // return rejectWithValue(err.response.data.data);
+      return rejectWithValue(err.message);
+    });
+  return response;
+};
 
-  export const settingsAPI =async (lang, { rejectWithValue }) => {
-    const response = await api
-      .get("/settings", {
-        headers: {
-          locale: lang,
-        },
-      })
-      .then((res) => {
-        // console.log(res.data.data);
-        return res.data.data;
-      })
-      .catch((err) => {
-        // console.log(err.response)
-        return rejectWithValue(err.message);
-      });
-    return response;
-  }
+export const settingsAPI = async (lang, { rejectWithValue }) => {
+  const response = await api
+    .get("/settings", {
+      headers: {
+        locale: lang,
+      },
+    })
+    .then((res) => {
+      // console.log(res.data.data);
+      return res.data.data;
+    })
+    .catch((err) => {
+      // console.log(err.response)
+      return rejectWithValue(err.message);
+    });
+  return response;
+};
